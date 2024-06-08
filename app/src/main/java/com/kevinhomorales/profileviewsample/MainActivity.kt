a8cb48694d80f86f3d8a4da2ac792c6ebda39c2c
Kevin Morales <kfmorales94@gmail.com>
2024-06-08 13:49:24 -0500

    Project done

--- app/src/main/java/com/kevinhomorales/profileviewsample/MainActivity.kt
+++ app/src/main/java/com/kevinhomorales/profileviewsample/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.kevinhomorales.profileviewsample
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.kevinhomorales.profileviewsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpView()
+    }
+
+    private fun setUpView() {
+        setUpActions()
+    }
+
+    private fun setUpActions() {
+        binding.buttonId.setOnClickListener {
+            binding.cedulaId.setText("1717893901")
+            binding.nameId.setText("Kevin")
+            binding.lastNameId.setText("Morales")
+            binding.emailId.setText("kfmorales94@gmail.com")
+            binding.ageId.setText("29 años")
+            binding.cityId.setText("Santo Domingo")
+            binding.phoneId.setText("+593959084920")
+            binding.imageViewId.setImageResource(R.drawable.kevin)
         }
     }
 }
